b08743c903c022b2898a43ca2628f05d4738242b
winnerk0624 <110490315+winnerk0624@users.noreply.github.com>
2026-02-25 00:52:53 -0500

    Update data directories

--- 02_calc_cci.py
+++ 02_calc_cci.py
@@ -1,6 +1,6 @@
 ### specify directories ###
-data_directory = "Z:/Katie/Temperature_CLIF/SOFA_calc"
-output_directory = "Z:/Katie/Temperature_CLIF/SOFA_calc"
+data_directory = "Z:/Katie/Temperature_CLIF/Temperature_Variation_Suspected_Sepsis/icu-clif_data_processed"
+output_directory = "Z:/Katie/Temperature_CLIF/Temperature_Variation_Suspected_Sepsis/icu-clif_data_processed"
 
 from clifpy.tables.hospital_diagnosis import HospitalDiagnosis
 from clifpy.utils.comorbidity import calculate_cci
@@ -18,4 +18,5 @@ cci_results = calculate_cci(hosp_dx, hierarchy = True)
 
 # export cci_results
 cci_results.to_csv(f"{output_directory}/cci_results.csv", index=False)
+
 cci_results.to_parquet(f"{output_directory}/cci_results.parquet", index=False)
